f20681f077deb1f939eedae06971de94ab1e98cb
Sowhom Ghosh <sowhom.ghosh.22@aot.edu.in>
2025-09-11 22:32:01 +0530

    creating new movie object in database at BE1.1_CW folder

--- BE1.1_CW/index.js
+++ BE1.1_CW/index.js
@@ -1,33 +1,31 @@
-const { initializeDatabase } = require("./db/db.connect.js");
-const fs = require("fs");
-const Movie = require("./models/movie.models.js")
+const { initializeDatabase } = require("./db/db.connect");
+const Movie = require("./models/movie.models")
 
-const jsonData = fs.readFileSync("movies.json", "utf-8");
-const moviesData = JSON.parse(jsonData);
+initializeDatabase();
 
-async function seedData() {
-    await initializeDatabase();
+const newMovie = {
+  title: "New Movie",
+  releaseYear: 2023,
+  genre: ["Drama"],
+  director: "Aditya Roy Chopra",
+  actors: ["Actor1", "Actor2"],
+  language: "Hindi",
+  country: "India",
+  rating: 6.1,
+  plot: "A young man and woman fall in love on a Australia trip.",
+  awards: "IFA Filmfare Awards",
+  posterUrl: "https://example.com/new-poster1.jpg",
+  trailerUrl: "https://example.com/new-trailer1.mp4"
+}
+
+async function createMovie(newMovie) {
   try {
-    for (const movieData of moviesData) {
-      const newMovie = new Movie({
-        title: movieData.title,
-        releaseYear: movieData.releaseYear,
-        genre: movieData.genre,
-        director: movieData.director,
-        actors: movieData.actors,
-        language: movieData.language,
-        country: movieData.country,
-        rating: movieData.rating,
-        plot: movieData.plot,
-        awards: movieData.awards,
-        posterUrl: movieData.posterUrl,
-        trailerUrl: movieData.trailerUrl,
-      });
-       await newMovie.save();
-      console.log(newMovie.title);
-    }
+    const movie = new Movie(newMovie)
+    const saveMovie = await movie.save()
+    console.log("New Movie data:", saveMovie)
   } catch (error) {
-    console.log("Error seeding the data", error);
+    throw error
   }
 }
-seedData();
+
+createMovie(newMovie)
