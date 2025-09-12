f443cf6f62d339a084be08dcdf8b16f7938b0b99
Sowhom Ghosh <sowhom.ghosh.22@aot.edu.in>
2025-09-12 10:08:44 +0530

    BE2.2_HW1

--- BE2.1_HW1/index.js
+++ BE2.1_HW1/index.js
@@ -1,32 +1,102 @@
 const { initializeDatabase } = require("./db/db.connect");
-const Restaurant = require("./models/restaurant.models")
+const Restaurant = require("./models/restaurant.models");
 
 initializeDatabase();
 
 const newRestaurant = {
-  name: "Cha Cha",
-  cuisine: ["Spanish"],
-  location: "123 Main Street, Anytown",
-  rating: 4.0,
+  name: "Yo China",
+  cuisine: ["Chinese", "Italian"],
+  location: "MG Road, Bangalore",
+  rating: 3.9,
   reviews: [],
-  website: "https://example.com",
-  phoneNumber: "+1234567890",
-  openHours: "Mon-Sun: 11:00 AM - 10:00 PM",
-  priceRange: "$$ (11-30)",
+  website: "https://yo-example.com",
+  phoneNumber: "+1288997392",
+  openHours: "Tue-Sun: 10:00 AM - 11:00 PM",
+  priceRange: "$$$ (31-60)",
   reservationsNeeded: true,
-  isDeliveryAvailable: true,
-  menuUrl: "https://example.com/menu",
-  photos: ["https://example.com/photo1.jpg", "https://example.com/photo2.jpg"],
+  isDeliveryAvailable: false,
+  menuUrl: "https://yo-example.com/menu",
+  photos: [
+    "https://example.com/yo-photo1.jpg",
+    "https://example.com/yo-photo2.jpg",
+    "https://example.com/yo-photo3.jpg",
+  ],
 };
 
+// Creating new restaurant data.
 async function createRestaurant(newRestaurant) {
   try {
-    const restaurant = new Restaurant(newRestaurant)
-    const saveRestaurant = await restaurant.save()
-    console.log("New Restaurant data:", saveRestaurant)
+    const restaurant = new Restaurant(newRestaurant);
+    const saveRestaurant = await restaurant.save();
+    console.log("New Restaurant data:", saveRestaurant);
   } catch (error) {
-    throw error
+    throw error;
   }
 }
 
-createRestaurant(newRestaurant)
+createRestaurant(newRestaurant);
+
+// Consoling all Restaurants
+async function allRestaurants() {
+  try {
+    const all = await Restaurant.find();
+    console.log(all);
+  } catch (error) {
+    throw error;
+  }
+}
+allRestaurants();
+
+// 4. Create a function to read a restaurant by its name ("New Restaurant"). Console the restaurant details. Use proper function and variable names.
+async function restaurantsByName(restaurantName) {
+  try {
+    const all = await Restaurant.findOne({name: restaurantName});
+    console.log(all);
+  } catch (error) {
+    throw error;
+  }
+}
+restaurantsByName("New Restaurant");
+
+// 5. Create a function to read all restaurants which offers reservations. Console the restaurant details.
+async function reservationRestaurants() {
+  try {
+    const all = await Restaurant.find({reservationsNeeded: true});
+    console.log(all);
+  } catch (error) {
+    throw error;
+  }
+}
+reservationRestaurants();
+
+// 6. Create a function to read all restaurants which offers delivery. Console the restaurant details.
+async function offersDeliveryRestaurants() {
+  try {
+    const all = await Restaurant.find({isDeliveryAvailable: true});
+    console.log(all);
+  } catch (error) {
+    throw error;
+  }
+}
+offersDeliveryRestaurants();
+
+// 7. Create a function to read a restaurant by phone number (+1288997392). Console the restaurant details.
+async function restaurantsByPhoneNumber(phone) {
+  try {
+    const all = await Restaurant.findOne({phoneNumber: phone});
+    console.log(all);
+  } catch (error) {
+    throw error;
+  }
+}
+restaurantsByPhoneNumber(1288997392);
+// 8. Create a function to read all restaurants by cuisine ("Italian"). Console all the restaurants with Italian cuisine.
+async function italianCuisineRestaurant() {
+  try {
+    const all = await Restaurant.find({cuisine: "Italian"});
+    console.log(all);
+  } catch (error) {
+    throw error;
+  }
+}
+italianCuisineRestaurant();
