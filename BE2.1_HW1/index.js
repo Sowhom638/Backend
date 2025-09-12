691f0b35362bb1854cf6ad86d70f8a51d0df2e1b
Sowhom Ghosh <sowhom.ghosh.22@aot.edu.in>
2025-09-12 18:10:39 +0530

    BE2.3_HW1

--- BE2.1_HW1/index.js
+++ BE2.1_HW1/index.js
@@ -34,7 +34,7 @@ async function createRestaurant(newRestaurant) {
   }
 }
 
-createRestaurant(newRestaurant);
+// createRestaurant(newRestaurant);
 
 // Consoling all Restaurants
 async function allRestaurants() {
@@ -45,7 +45,7 @@ async function allRestaurants() {
     throw error;
   }
 }
-allRestaurants();
+// allRestaurants();
 
 // 4. Create a function to read a restaurant by its name ("New Restaurant"). Console the restaurant details. Use proper function and variable names.
 async function restaurantsByName(restaurantName) {
@@ -56,7 +56,7 @@ async function restaurantsByName(restaurantName) {
     throw error;
   }
 }
-restaurantsByName("New Restaurant");
+// restaurantsByName("New Restaurant");
 
 // 5. Create a function to read all restaurants which offers reservations. Console the restaurant details.
 async function reservationRestaurants() {
@@ -67,7 +67,7 @@ async function reservationRestaurants() {
     throw error;
   }
 }
-reservationRestaurants();
+// reservationRestaurants();
 
 // 6. Create a function to read all restaurants which offers delivery. Console the restaurant details.
 async function offersDeliveryRestaurants() {
@@ -78,7 +78,7 @@ async function offersDeliveryRestaurants() {
     throw error;
   }
 }
-offersDeliveryRestaurants();
+// offersDeliveryRestaurants();
 
 // 7. Create a function to read a restaurant by phone number (+1288997392). Console the restaurant details.
 async function restaurantsByPhoneNumber(phone) {
@@ -89,7 +89,7 @@ async function restaurantsByPhoneNumber(phone) {
     throw error;
   }
 }
-restaurantsByPhoneNumber(1288997392);
+// restaurantsByPhoneNumber(1288997392);
 // 8. Create a function to read all restaurants by cuisine ("Italian"). Console all the restaurants with Italian cuisine.
 async function italianCuisineRestaurant() {
   try {
@@ -99,4 +99,40 @@ async function italianCuisineRestaurant() {
     throw error;
   }
 }
-italianCuisineRestaurant();
+// italianCuisineRestaurant();
+
+// 1. Create a function that accepts a restaurant ID and an object with updated data, and updates the restaurant with the provided ID. Take the _id of the restaurant which has the name Yo China and update its rating from 3.9 to 4.1. Console the updated restaurant.
+async function updateRating(objectId, updation) {
+  try {
+    const all = await Restaurant.findByIdAndUpdate(objectId, updation, { new: true });
+    console.log(all);
+  } catch (error) {
+    console.log("Error while update rating", error);
+  
+  }
+}
+// updateRating('68c39d327fc29f1572701438', {rating: 4.1});
+
+// 2. Create a function that accepts a restaurant name and an object with updated data, and updates the restaurant. Take the restaurant which has the name "Somi" and update its name from "Somi" to "Som Sarovar". Console the updated restaurant.
+async function findByName(restName, updation) {
+  try {
+    const all = await Restaurant.findOneAndUpdate({name: restName}, updation, { new: true });
+    console.log(all);
+  } catch (error) {
+    console.log("Error while update name", error);
+  
+  }
+}
+// findByName("Somi", {name: "Som Sarovar"})
+
+// Create a function that accepts a restaurant's phone number and an object with updated data, and updates the restaurant. Take the restaurant which has the phone number "+1288997392" and update isDeliveryAvailable option to true. Console the updated restaurant.
+async function findByPhoneNumber(phnNum, updation) {
+  try {
+    const all = await Restaurant.findOneAndUpdate({phoneNumber: phnNum}, updation, { new: true });
+    console.log(all);
+  } catch (error) {
+    console.log("Error while update delivery status", error);
+  
+  }
+}
+findByPhoneNumber(1288997392, {isDeliveryAvailable: true})
