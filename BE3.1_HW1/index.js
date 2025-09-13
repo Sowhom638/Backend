89e72a8ae87c5d5aba8fb331bccf8d8f1112dc1f
Sowhom Ghosh <sowhom.ghosh.22@aot.edu.in>
2025-09-13 15:12:48 +0530

    BE3.1_HW1 Added

---
+++ BE3.1_HW1/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res)=>{
+    res.send("Hello, Express JS");
+});
+app.get("/products", (req, res)=>{
+    res.send( "Browse our products here.");
+});
+app.get("/services", (req, res)=>{
+    res.send("Explore our services.");
+});
+app.get("/faq", (req, res)=>{
+    res.send( "Frequently Asked Questions.");
+});
+app.get("/gallery", (req, res)=>{
+    res.send( "View our gallery.");
+});
+
+const Port = process.env.PORT || 3000;
+
+app.listen(Port, ()=>{
+    console.log("Server is running on port", Port);
+    
+})
