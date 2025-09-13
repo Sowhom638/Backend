b46838e5eeb1e67da0efe93153500da6c677cbfe
Sowhom Ghosh <sowhom.ghosh.22@aot.edu.in>
2025-09-13 15:22:06 +0530

    BE3.1_HW2

---
+++ BE3.1_HW2/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from express server.");
+});
+app.get("/signin", (req, res) => {
+  res.send("This is the Sign In page.");
+});
+app.get("/booking", (req, res) => {
+  res.send("Book your tickets here.");
+});
+app.get("/clothing/kids", (req, res) => {
+  res.send("This is the kids wear page.");
+});
+app.get("/blog", (req, res) => {
+  res.send("This is the blog page.");
+});
+
+const Port = process.env.PORT || 3000;
+
+app.listen(Port, () => {
+  console.log("Server is running on port", Port);
+});
