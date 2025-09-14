e0303a4c4c9fc0b77a43376dd473610cc2c1211e
Sowhom Ghosh <sowhom.ghosh.22@aot.edu.in>
2025-09-14 22:01:43 +0530

    BE3.4_CW added

--- BE3.3_CW/index.js
+++ BE3.3_CW/index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 const cars = [
 { id: 1, make: "Toyota", model: "Camry", year: 2022 },
 { id: 2, make: "Honda", model: "Civic", year: 2021 },
-{ id: 3, make: "Ford", model: "Mustang", year: 2022 },
+{ id: 12, make: "Kia", model: "Seltos", year: 2022 },
 { id: 4, make: "Chevrolet", model: "Corvette", year: 2023 },
 { id: 5, make: "Tesla", model: "Model 3", year: 2021 },
 { id: 6, make: "Nissan", model: "Altima", year: 2022 },
@@ -16,7 +16,7 @@ const cars = [
 { id: 9, make: "Audi", model: "A4", year: 2022 },
 { id: 10, make: "Lexus", model: "RX", year: 2023 },
 { id: 11, make: "Hyundai", model: "Tucson", year: 2021 },
-{ id: 12, make: "Kia", model: "Seltos", year: 2022 },
+{ id: 3, make: "Ford", model: "Mustang", year: 2022 },
 { id: 13, make: "Mazda", model: "CX-5", year: 2023 },
 { id: 14, make: "Subaru", model: "Outback", year: 2021 },
 { id: 15, make: "Volkswagen", model: "Golf", year: 2022 }
@@ -42,14 +42,30 @@ res.send(cars);
 });
 
 app.delete("/cars/:id", (req, res) => {
-const carId = req.params.id;
+const carId = parseInt(req.params.id); // converting the string into integer
 
-const index = cars.findIndex((car) => car.id == carId);
+const index = cars.findIndex((car) => car.id === carId);
 if(index === -1){
   res.status(404).json({ error: "Car not Found." });
 } else {
-  cars.splice(index,1);
+  cars.splice(index,1); // .splice(a, b) => delete 'b' elements starting from index 'a' of the array
   res.status(200).json({ message: "Car deleted successfully."});
+}
+});
+app.post("/cars/:id", (req, res) => {
+const carId = parseInt(req.params.id); // converting the string into integer
+const updatedCarData = req.body;
+
+const carToUpdate = cars.find((car) => car.id === carId);
+if(!carToUpdate){
+  res.status(404).json({ error: "Car not Found." });
+} else {
+  if(!updatedCarData.make || !updatedCarData.model || !updatedCarData.year){
+    res.status(400).json({ error: "make, model and year are required." });
+  }else{
+    Object.assign(carToUpdate, updatedCarData); // Object.assign(destinationObject, sourceObject) --> To copy object data from source object to destination object
+  res.status(200).json({ message: "Car data updated successfully.", car: carToUpdate});
+  }
 }
 });
 
