6f52b13699c5a441d09156ef7b4b111e4e7f90bd
Sowhom Ghosh <sowhom.ghosh.22@aot.edu.in>
2025-09-14 22:45:21 +0530

    BE3.4_HW2 Updated

--- BE3.3_HW2/index.js
+++ BE3.3_HW2/index.js
@@ -47,11 +47,28 @@ if(index === -1){
 }
 });
 
+app.post("/movies/:id", (req, res) => {
+const movieId = parseInt(req.params.id); // converting the string into integer
+const updatedMovieData = req.body;
+
+const movieToUpdate = movies.find((movie) => movie.id === movieId);
+if(!movieToUpdate){
+  res.status(404).json({ error: "Movie not Found." });
+} else {
+  if(!updatedMovieData.title || !updatedMovieData.director || !updatedMovieData.year){
+    res.status(400).json({ error: "title, director and year are required." });
+  }else{
+    Object.assign(movieToUpdate, updatedMovieData); // Object.assign(destinationObject, sourceObject) --> To copy object data from source object to destination object
+  res.status(200).json({ message: "Movie data updated successfully.", movie: movieToUpdate});
+  }
+}
+});
+
 app.post("/items", (req, res) => {
 const newItem = req.body;
 
-if (!newItem.title || !newItem.day) {
-res.status(400).json({ error: "title and day are required." });
+if (!newItem.itemName || !newItem.color || !newItem.quantity) {
+res.status(400).json({ error: "itemName, color and quantity are required." });
 } else {
 items.push(newItem);
 res.status(201).json({ message: "item added successfully.", item: newItem });
@@ -71,6 +88,23 @@ if(index === -1){
 } else {
   items.splice(index,1);
   res.status(200).json({ message: "Item deleted successfully."});
+}
+});
+
+app.post("/items/:id", (req, res) => {
+const itemId = parseInt(req.params.id); // converting the string into integer
+const updatedItemData = req.body;
+
+const itemToUpdate = items.find((item) => item.id === itemId);
+if(!itemToUpdate){
+  res.status(404).json({ error: "Item  not found." });
+} else {
+  if(!updatedItemData.itemName || !updatedItemData.color || !updatedItemData.quantity){
+    res.status(400).json({ error: "itemName, color and quantity are required." });
+  }else{
+    Object.assign(itemToUpdate, updatedItemData); // Object.assign(destinationObject, sourceObject) --> To copy object data from source object to destination object
+  res.status(200).json({ message: "Item data updated successfully.", item: itemToUpdate});
+  }
 }
 });
 
