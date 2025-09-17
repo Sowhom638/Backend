061dcc8b41ab24ad6c53edf8553b423ecb9cb771
Sowhom Ghosh <sowhom.ghosh.22@aot.edu.in>
2025-09-17 10:44:48 +0530

    BE4.4_CW Added

--- BE4.1_CW/index.js
+++ BE4.1_CW/index.js
@@ -132,6 +132,29 @@ res.status(404).json({ error: "Movie not Found." });
   }
 })
 
+async function updateMovie(movieId, dataToUpdate) {
+    try {
+        const updatedMovie = await Movie.findByIdAndUpdate(movieId, dataToUpdate, {
+            new: true,
+        });
+        return updatedMovie;
+    } catch (error) {
+        console.log("Error in updating Movie rating", error);
+    }
+}
+app.post("/movies/:movieId", async (req, res) => {
+    try {
+        const updatedMovie = await updateMovie(req.params.movieId, req.body);
+        if (updatedMovie) {
+            res.status(200).json({ message: "Movie updated successfully.", updatedMovie });
+        } else {
+            res.status(404).json({ error: "Movie not found." });
+        }
+    } catch (error) {
+        res.status(500).json({ error: "failed to update movie." });
+    }
+});
+
 async function deleteMovie(movieId) {
     try {
         const deletedMovie = await Movie.findByIdAndDelete(movieId);
