b0bfe8870b77235272c0a7459d5fb70ba4cc0002
Sowhom Ghosh <sowhom.ghosh.22@aot.edu.in>
2025-09-22 01:43:50 +0530

    BI1.3_HW2 Added

--- BI1.1_HW2/frontend/src/components/Hotel.jsx
+++ BI1.1_HW2/frontend/src/components/Hotel.jsx
@@ -1,20 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
 import useFetch from "../../useFetch";
 
 const Hotel = () => {
+    const [successMessage, setSuccessMessage] = useState("");
   const { data, loading, error } = useFetch(
     `http://localhost:3000/hotels`
   );
-  console.log(data);
+  const handleDelete = async (hotelId) => {
+    try {
+      const response = await fetch(`http://localhost:3000/hotels/${hotelId}`, {
+        method: "DELETE",
+      });
+      if (response.ok) {
+        const data = await response.json();
+        if (data) {
+          setSuccessMessage("Hotel deleted successfully");
+
+          window.location.reload();
+        }
+      } else {
+        throw "Failed to delete hotel";
+      }
+    } catch (error) {
+      console.log("Error: ", error);
+    }
+  };
 
   return data ? (
     <div>
       <h2>All Hotels</h2>
         <ul>
       {data && data.hotel.map(Hotel=>(
-        <li key={Hotel._id}>{Hotel.name}</li>
+        <li key={Hotel._id}>
+          {Hotel.name} <button onClick={() => handleDelete(Hotel._id)}>Delete</button>
+        </li>
       ))}
       </ul>
+      {successMessage != "" && <p>{successMessage}</p>}
     </div>
   ) : (
     <div>{loading && <p>loading...</p>}</div>
