258ef44fd73a6f6776ded183c453dc725881df14
Sowhom Ghosh <sowhom.ghosh.22@aot.edu.in>
2025-09-22 01:01:50 +0530

    BI1.3_CW Added

--- BI1.1_CW/frontend/src/components/Movie.jsx
+++ BI1.1_CW/frontend/src/components/Movie.jsx
@@ -1,17 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
 import useFetch from '../../useFetch'
 
 const Movie = () => {
+  const [successMessage, setSuccessMessage] = useState("")
     const {data, loading, error} = useFetch("http://localhost:3000/movies");
 console.log(data);
 
+const handleDelete = async (movieId) => {
+  try {
+    const response =  await fetch(`http://localhost:3000/movies/${movieId}`, {
+    method: "DELETE"
+  })
+  if(response.ok) {
+    const data = await response.json();
+    if(data){
+    setSuccessMessage("Movie deleted successfully");
+
+    window.location.reload();
+    }
+  }else{
+    throw "Failed to delete movie"
+  }
+  } catch (error) {
+    console.log("Error: ", error);
+    
+  }
+  
+}
   return (
     <div>
       <ul>
         {data && data.movie.map(data => (
-          <li key={data._id}>{data.title}</li>
+          <li key={data._id}>
+            {data.title} <button onClick={()=>handleDelete(data._id)}>Delete</button>
+            </li>
         ))}
       </ul>
+      {successMessage != "" && <p>{successMessage}</p>}
     </div>
   )
 }
